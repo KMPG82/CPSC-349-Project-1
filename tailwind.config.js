2ea48a7a7b8d77a632b1c8377fdf18c6a969d6f1
Kevin <kevinponting82@gmail.com>
2023-09-27 19:49:29 -0700

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "About_Us_Page.html",
+    "About_Us_Page.css"
+  ],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
